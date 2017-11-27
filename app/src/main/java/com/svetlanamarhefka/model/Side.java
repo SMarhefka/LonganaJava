4267b33a39c44961a4e4c7b86ce9827cf8cce82f
Svetlana Marhefka <svetlana marhefka@smarhefka>
2017-11-26 21:49:52 -0500

    Sunday November 26th 2017 Model - Side   - created a new enum class called Side which just contains Left, Right     or Either.  This class was created to make user input and comparison     easier Views - BoardView (30 minutes)   - created a new displayBoard() method which is responsible for updating     the main gameboard - BoneyardView (10 minutes)   - created a new displayBoneyard() method wich is responsible for     displaying the dominoes left in the boneyard (these are simply images) - HumanView (1 hour)   - this took some time because I didn't know how to add clickable buttons   - created btnDominoUpDwn() (**Have to remember to change the name of     the method).  This method creates a clickable dominoTile.

---
+++ app/src/main/java/com/svetlanamarhefka/model/Side.java
@@ -0,0 +1,14 @@
+package com.svetlanamarhefka.model;
+
+import java.io.Serializable;
+
+/****************************************************************
+ * Name:    Svetlana Marhefka                                             *
+ * Project: Project 2 - Longana                                 *
+ * Class:   CMPS 366 Organization of Programming Languages (OPL)* 
+ * Date:    11/26/2017                                             *
+ ****************************************************************/
+
+public enum Side implements Serializable{
+    LEFT, RIGHT, EITHER
+}
---
+++ app/src/main/java/com/svetlanamarhefka/view/BoardView.java
@@ -0,0 +1,58 @@
+package com.svetlanamarhefka.view;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+
+import com.svetlanamarhefka.model.Domino;
+
+import java.util.Vector;
+
+/****************************************************************
+ * Name:    Svetlana Marhefka                                             *
+ * Project: Project 2 - Longana                                 *
+ * Class:   CMPS 366 Organization of Programming Languages (OPL)* 
+ * Date:    11/26/2017                                             *
+ ****************************************************************/
+
+public class BoardView extends View {
+
+    private DominoView m_DominoView;
+    private Context m_Context;
+
+    BoardView(Context context)
+    {
+        super(context);
+        m_Context = context;
+        m_DominoView = new DominoView(m_Context);
+    }
+
+    public void displayBoard(Vector<Domino> a_InDomino, LinearLayout a_InLayout)
+    {
+        GridLayout.LayoutParams gridLayoutParam;
+
+        int row = 0, col = 0;
+        for (int i = 0; i < a_InDomino.size(); i++) {
+            if (i >= 14) row = 1;
+            col = i % 14;
+            GridLayout.Spec gridRow = GridLayout.spec(row, 1);
+            GridLayout.Spec gridCol = GridLayout.spec(col, 1);
+            gridLayoutParam = new GridLayout.LayoutParams(gridRow, gridCol);
+            gridLayoutParam.setMargins(4, 0, 4, 0);
+
+            if(a_InDomino.get(i).isDouble())
+            {
+                a_InLayout.addView(m_DominoView.imgDominoUpDwn (a_InDomino.get(i)), gridLayoutParam);
+            }
+            if (a_InDomino.get(i).getM_leftSide() < a_InDomino.get(i).getM_rightSide())
+            {
+                a_InLayout.addView(m_DominoView.imgDominoAccross (a_InDomino.get(i), false), gridLayoutParam);
+            }
+            else
+            {
+                a_InLayout.addView(m_DominoView.imgDominoAccross (a_InDomino.get(i), true), gridLayoutParam);
+            }
+        }
+    }
+}
